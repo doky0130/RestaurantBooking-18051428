c1077303f8431ea9ec87a98f10342be0ab40eb63
Kim Dokyoung <doky0130@gmail.com>
2025-07-24 12:53:19 +0900

    [feature] 정각 여부에 따른 assertion or 예약 성공 여부 unit test 작성 및 Pass 확인 완료

--- 20250724_RestaurantBooking/test.cpp
+++ 20250724_RestaurantBooking/test.cpp
@@ -2,11 +2,39 @@
 #include "booking_scheduler.cpp"
 
 TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
+	tm notOnTheHour = { 0 };
+	notOnTheHour.tm_year = 2025 - 1900;
+	notOnTheHour.tm_mon = 7 - 1;
+	notOnTheHour.tm_mday = 24;
+	notOnTheHour.tm_hour = 12;
+	notOnTheHour.tm_min = 45;
+	notOnTheHour.tm_isdst = -1;
+	mktime(&notOnTheHour);
 
+	Customer customer{ "name","010-1234-5678" };
+	Schedule* schedule = new Schedule{ notOnTheHour , 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	EXPECT_THROW(bookingScheduler.addSchedule(schedule),
+		std::runtime_error);
 }
 
 TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
+	tm onTheHour = { 0 };
+	onTheHour.tm_year = 2025 - 1900;
+	onTheHour.tm_mon = 7 - 1;
+	onTheHour.tm_mday = 24;
+	onTheHour.tm_hour = 12;
+	onTheHour.tm_min = 0;
+	onTheHour.tm_isdst = -1;
+	mktime(&onTheHour);
 
+	Customer customer{ "name","010-1234-5678" };
+	Schedule* schedule = new Schedule{ onTheHour , 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	bookingScheduler.addSchedule(schedule);
+	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
 }
 
 TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
